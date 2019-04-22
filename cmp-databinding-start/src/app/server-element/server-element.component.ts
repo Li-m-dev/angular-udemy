16706d04ea390c8a5e9c4f66f5391360331963eb
Li Swafford <liswafford@yahoo.com>
2019-04-22 10:29:06 -0500

    lifecyle

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  Input,
+  ViewEncapsulation,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  ViewChild,
+  ElementRef
+} from "@angular/core";
 
 @Component({
   selector: "app-server-element",
@@ -8,11 +23,48 @@ import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
   //emulated is default, but you can also choose None or Native. when it is None, it will make the
   // styling of the file.css global
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy {
   @Input("srvElement") element: { type: string; name: string; content: string };
   //binding through alias srvElement
+  @Input() name: string;
+  @ViewChild("heading") header: ElementRef;
+  constructor() {
+    console.log("constructor called");
+  }
 
-  constructor() {}
-
-  ngOnInit() {}
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnchanges: ", changes);
+  }
+  ngOnInit() {
+    console.log("ngOnInit called");
+    console.log("text content" + this.header.nativeElement.textContent);
+  }
+  ngDoCheck() {
+    console.log("ngDoCheck called");
+  }
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit called");
+  }
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked called");
+  }
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked called");
+  }
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit called");
+    console.log("text content" + this.header.nativeElement.textContent);
+  }
+  ngOnDestroy() {
+    console.log("ngOnDestroy called");
+  }
 }
