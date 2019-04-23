ae416e42574df768a99632979489ba1b02d8ca10
Li Swafford <liswafford@yahoo.com>
2019-04-23 15:43:18 -0500

    using EventEmitter to check if the array has changed

--- prj-start/src/app/shopping-list/shopping-list.service.ts
+++ prj-start/src/app/shopping-list/shopping-list.service.ts
@@ -1,1 +1,21 @@
-export class shoppingListService {}
+import { Ingredient } from "../shared/ingredient.model";
+import { EventEmitter } from "@angular/core";
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  // ingredientsChanged = {
+  //   emit: function(param:Ingredient[]){},
+
+  // }
+  private ingredients: Ingredient[] = [
+    new Ingredient("Apple", 5),
+    new Ingredient("tomatoes", 10)
+  ];
+
+  getIngredient() {
+    return this.ingredients.slice();
+  }
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+}
