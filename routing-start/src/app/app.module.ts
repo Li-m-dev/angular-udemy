c6a7e7199eb09f384d8379670216063348f3e212
Li Swafford <liswafford@yahoo.com>
2019-04-24 11:16:14 -0500

    protect route

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersService } from "./servers/servers.service";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { AuthGuard } from "./auth-guard.service";
+import { AuthService } from "./auth.service";
 
 const appRoute: Routes = [
   { path: "", component: HomeComponent, pathMatch: "full" },
@@ -23,6 +25,8 @@ const appRoute: Routes = [
   },
   {
     path: "servers",
+    // canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
     component: ServersComponent,
     children: [
       { path: ":id", component: ServerComponent },
@@ -49,7 +53,7 @@ const appRoute: Routes = [
     HttpModule,
     RouterModule.forRoot(appRoute)
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthGuard, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ routing-start/src/app/auth-guard.service.ts
@@ -0,0 +1,35 @@
+import {
+  CanActivate,
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  CanActivateChild
+} from "@angular/router";
+import { AuthService } from "./auth.service";
+import { Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.isAuthenticated().then((authenticated: boolean) => {
+      if (authenticated) {
+        return true;
+      } else {
+        this.router.navigate(["/"]);
+      }
+    });
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate(route, state);
+  }
+}
