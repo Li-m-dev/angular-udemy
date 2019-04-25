40fa48fe0f62e3817361aa449265690ca2652240
Li Swafford <liswafford@yahoo.com>
2019-04-25 11:32:25 -0500

    [ngModel]set up one way data binding, #whatever give you the local reference

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  @ViewChild("f") userForm: NgForm;
+  subscriptionTypes = ["Basic", "Advanced", "Pro"];
+  defaultOption = "Advanced";
+  user = {
+    address: "",
+    password: "",
+    subscriptionType: ""
+  };
+  submitted = false;
 
+  onSubmit() {
+    console.log("here", this.userForm.value);
+    this.submitted = true;
+    this.user.address = this.userForm.value.address;
+    this.user.password = this.userForm.value.password;
+    this.user.subscriptionType = this.userForm.value.subscriptionType;
+  }
 }
