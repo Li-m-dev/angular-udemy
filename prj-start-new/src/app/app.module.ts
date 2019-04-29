a2231380df35ae762a06e5d5edb0f9e0e4c5d01c
Li Swafford <liswafford@yahoo.com>
2019-04-29 16:38:26 -0500

    add lazy loading

--- prj-start-new/src/app/app.module.ts
+++ prj-start-new/src/app/app.module.ts
@@ -7,17 +7,17 @@ import { HeaderComponent } from "./header/header.component";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { AppRoutingModule } from "./app-routing.module";
 import { RecipeService } from "./recipes/recipe.service";
-import { RecipesModule } from "./recipes/recipes.module";
 import { SharedModule } from "./shared/shared.module";
 import { SlModule } from "./shopping-list/shopping-list.module";
 import { AuthModule } from "./auth/auth.module";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
   imports: [
     BrowserModule,
     HttpModule,
-    RecipesModule,
+
     AppRoutingModule,
     SharedModule,
     SlModule,
