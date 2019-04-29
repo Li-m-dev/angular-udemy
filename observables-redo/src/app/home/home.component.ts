0850fb612113ef9dde5b968ee7125d4fcd8f911a
Li Swafford <liswafford@yahoo.com>
2019-04-28 19:40:32 -0500

    Observable, Subject

--- observables-redo/src/app/home/home.component.ts
+++ observables-redo/src/app/home/home.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Observable, Observer, Subscription, interval } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  numbersObsSubscription: Subscription;
+  customObsSubscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    const myNumbers = interval(1000).pipe(
+      map((data: number) => {
+        return data * 2;
+      })
+    );
+    this.numbersObsSubscription = myNumbers.subscribe((number: Number) => {
+      console.log("number: ", number);
+    });
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next("first package");
+      }, 2000);
+      setTimeout(() => {
+        observer.next("second package");
+      }, 4000);
+      setTimeout(() => {
+        // observer.error("this does not work");
+        observer.complete();
+      }, 5000);
+    });
+    this.customObsSubscription = myObservable.subscribe(
+      (data: string) => {
+        console.log("data: ", data);
+      },
+      (error: string) => {
+        console.log("error: ", error);
+      },
+      () => {
+        console.log("complete");
+      }
+    );
   }
-
+  ngOnDestroy() {
+    this.numbersObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
+  }
 }
